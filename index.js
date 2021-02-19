88f99bbde20350c7f3176958304176d7b74d094e
will <wjbeshore@gmail.com>
2021-02-19 15:16:07 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+const path = require('path');
+
+const express = require('express');
+const expressEdge = require('express-edge');
+ 
+const app = new express();
+
+ 
+app.use(express.static('public'));
+app.use(expressEdge.engine);
+app.set('views', __dirname + '/public/views');
+ 
+app.get('/', (req, res) => {
+    res.render('index');
+});
+ 
+
+
+
+ 
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
+
+ 
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
+ 
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+});
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000')
+});
